4ab9911c799a6b748e6b6108f29a6188a46aa4f9
jihyun <pagwa334@gmail.com>
2024-11-28 22:41:00 +0900

    테스트결과 페이지 생성

--- src/pages/testResult.jsx
+++ src/pages/testResult.jsx
@@ -1,9 +1,35 @@
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
+import { getTestResults } from "../api/testResults";
+import TestResultItem from "../components/TestResultItem";
+import { UserContext } from "../context/UserContext";
 
 const TestResult = () => {
+  const { user } = useContext(UserContext);
+  const [results, setResults] = useState([]);
+
+  const fetchResults = async () => {
+    const data = await getTestResults();
+    const filteredData = data.filter(
+      (item) => item.visibility || item.userId === user.id
+    );
+    setResults(filteredData);
+  };
+
+  useEffect(() => {
+    fetchResults();
+  }, []);
+
   return (
     <div>
-      <h1>테스트 결과 페이지</h1>
+      <h1>테스트 결과 목록</h1>
+      {results.map((result) => (
+        <TestResultItem
+          key={result.id}
+          result={result}
+          user={user}
+          refreshResults={fetchResults}
+        />
+      ))}
     </div>
   );
 };
